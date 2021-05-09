7efa49298591576761d6a953ceb31f52ca4d3310
Max Klenk <max@klenk.biz>
2021-05-09 20:29:21 +0200

    build

---
+++ precache-manifest.fd08633f03a67b66a82f4bce0d46a945.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa002117e0a5a365cceab1b9800ab78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4fbb1f9fa40c81671a0f",
+    "url": "./static/css/main.6f46186d.chunk.css"
+  },
+  {
+    "revision": "302735efba8151cc2e4a",
+    "url": "./static/js/2.6dbd2754.chunk.js"
+  },
+  {
+    "revision": "2845348b77af5d41d8e672b8d3f58fa8",
+    "url": "./static/js/2.6dbd2754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbb1f9fa40c81671a0f",
+    "url": "./static/js/main.7b4c0ff2.chunk.js"
+  },
+  {
+    "revision": "607911425784431aa9f5",
+    "url": "./static/js/runtime-main.aef6d470.js"
+  },
+  {
+    "revision": "22ba7daf55ae434609b8466430188afb",
+    "url": "./static/media/background.22ba7daf.png"
+  },
+  {
+    "revision": "745a993521fcf11b427acd4020935b99",
+    "url": "./static/media/bsc.745a9935.png"
+  },
+  {
+    "revision": "7ffdcbfe6ee6b8e7a08bc97894af8ac4",
+    "url": "./static/media/ethereum.7ffdcbfe.png"
+  },
+  {
+    "revision": "0dc6c0521a79b70dda2cea7ee3c42ead",
+    "url": "./static/media/quick.0dc6c052.png"
+  },
+  {
+    "revision": "5ab3a7154846db89ba89fa55a3cfcfd0",
+    "url": "./static/media/uniswap.5ab3a715.png"
+  }
+]);
